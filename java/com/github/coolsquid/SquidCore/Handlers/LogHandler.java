8d71744fddc8eac48169b2ddbf753b00bc5764ad
coolsquid <snakkeflink@gmail.com>
2014-11-30 22:27:45 +0100

    Create LogHandler.java

---
+++ java/com/github/coolsquid/SquidCore/Handlers/LogHandler.java
@@ -0,0 +1,41 @@
+package com.github.coolsquid.SquidCore.Handlers;
+
+import org.apache.logging.log4j.Level;
+
+import cpw.mods.fml.common.FMLLog;
+
+/**
+ * 
+ * @author CoolSquid
+ * All rights reserved.
+ *
+ */
+
+public final class LogHandler {
+
+	public static void info(String M, String message) {
+		FMLLog.log(M, Level.INFO, message);
+		}
+
+	public static void debug(String M, String message) {
+		FMLLog.log(M, Level.DEBUG, message);
+		}
+
+	public static void warn(String M, String message) {
+		FMLLog.log(M, Level.WARN, message);
+		}
+
+	public static void error(String M, String message) {
+		FMLLog.log(M, Level.ERROR, message);
+		}
+
+	public static void fatal(String M, String message) {
+		FMLLog.log(M, Level.FATAL, message);
+		}
+	
+	public static void bigWarning(String M, String message) {
+		FMLLog.log(M, Level.FATAL, "-------------------------------------------------------------------------------------------");
+		FMLLog.log(M, Level.FATAL, "!!!!!!!!!!!!!!!" + message + "!!!!!!!!!!!!!!!");
+		FMLLog.log(M, Level.FATAL, "-------------------------------------------------------------------------------------------");
+		}
+}

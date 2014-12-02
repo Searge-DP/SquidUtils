707680735022619dac948d4a1f58e0bb15088e16
coolsquid <snakkeflink@gmail.com>
2014-12-02 12:34:55 +0100

    Update SquidBoolean.java

--- java/com/github/coolsquid/SquidCore/utils/SquidBoolean.java
+++ java/com/github/coolsquid/SquidCore/utils/SquidBoolean.java
@@ -13,42 +13,9 @@ public class SquidBoolean {
 	
 	static Random rand = new Random();
 	
-	public static boolean half() {
+	public static boolean half(int I) {
 
-		int a = rand.nextInt(2) + 1;
-		if (a == 1) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-	
-	public static boolean third() {
-
-		int a = rand.nextInt(3) + 1;
-		if (a == 1) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-
-	public static boolean fourth() {
-
-		int a = rand.nextInt(4) + 1;
-		if (a == 1) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-	
-	public static boolean fifth() {
-
-		int a = rand.nextInt(5) + 1;
+		int a = rand.nextInt(I) + 1;
 		if (a == 1) {
 			return true;
 		}

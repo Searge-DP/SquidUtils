e9fbf1739f33427e803f7e231f6867cb661af410
coolsquid <snakkeflink@gmail.com>
2014-12-02 21:12:30 +0100

    Create RecipeHandler.java

---
+++ java/com/github/coolsquid/SquidCore/Handlers/RecipeHandler.java
@@ -0,0 +1,13 @@
+package com.github.coolsquid.SquidCore.Handlers;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.oredict.ShapedOreRecipe;
+import cpw.mods.fml.common.registry.GameRegistry;
+
+public class RecipeHandler {
+
+	public static void OreDictRecipe(String name) {
+		GameRegistry.addRecipe(new ShapedOreRecipe(Blocks.diamond_block, true, new Object[]{
+				"G", Character.valueOf('G'), name}));
+	}
+}

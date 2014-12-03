dd194057883d5892087552fe2dae54d28f880fb8
coolsquid <snakkeflink@gmail.com>
2014-12-03 18:00:20 +0100

    Update RecipeHandler.java

--- java/com/github/coolsquid/SquidUtils/Handlers/RecipeHandler.java
+++ java/com/github/coolsquid/SquidUtils/Handlers/RecipeHandler.java
@@ -1,6 +1,9 @@
 package com.github.coolsquid.SquidUtils.Handlers;
 
-import net.minecraft.init.Blocks;
+package com.github.coolsquid.SquidUtils.Handlers;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraftforge.oredict.ShapedOreRecipe;
 import cpw.mods.fml.common.registry.GameRegistry;
 
@@ -10,11 +13,16 @@ import cpw.mods.fml.common.registry.GameRegistry;
  * All rights reserved.
  *
  */
- 
+
 public class RecipeHandler {
 
-	public static void OreDictRecipe(String name) {
-		GameRegistry.addRecipe(new ShapedOreRecipe(Blocks.diamond_block, true, new Object[]{
-				"G", Character.valueOf('G'), name}));
+	public static void OreDictRecipeBlock(String input, Block output) {
+		GameRegistry.addRecipe(new ShapedOreRecipe(output, true, new Object[]{
+				"G", Character.valueOf('G'), input}));
+	}
+	
+	public static void OreDictRecipeItem(String input, Item output) {
+		GameRegistry.addRecipe(new ShapedOreRecipe(output, true, new Object[]{
+				"G", Character.valueOf('G'), input}));
 	}
 }

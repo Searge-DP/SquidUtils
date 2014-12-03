17196609b184032f011dfa4d503a32fda9f5cf66
coolsquid <snakkeflink@gmail.com>
2014-12-03 19:30:11 +0100

    Update and rename MFRHandler.java to ModHandler.java

--- java/com/github/coolsquid/SquidUtils/Handlers/ModHandler.java
+++ java/com/github/coolsquid/SquidUtils/Handlers/ModHandler.java
@@ -5,13 +5,20 @@ import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 /**
+package com.github.coolsquid.SquidUtils.Handlers;
+
+import net.minecraftforge.oredict.OreDictionary;
+//import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+
+/**
  * 
  * @author CoolSquid
  * All rights reserved.
  *
  */
  
-public class MFRHandler {
+public class ModHandler {
 	
 	@SubscribeEvent
 	public void oredict(OreDictionary.OreRegisterEvent event) {
@@ -23,9 +30,11 @@ public class MFRHandler {
 			}
 			//RecipeHandler.OreDictRecipeBlock("Greggy_greg_do_please_kindly_stuff_a_sock_in_it_xxx", Blocks.diamond_block);
 		}
+		/**
 		else if (event.Name.length() > 25) {
 			LogHandler.bigWarning("Too long oredictionary entry! This is not allowed due to SquidCore.");
 			FMLCommonHandler.instance().exitJava(1, false);
 		}
+		*/
 	}
 }

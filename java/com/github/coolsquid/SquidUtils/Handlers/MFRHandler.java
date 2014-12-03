824392c796fc41ef68755e5b8c6579a0d768acda
coolsquid <snakkeflink@gmail.com>
2014-12-03 12:13:47 +0100

    Update MFRHandler.java

--- java/com/github/coolsquid/SquidUtils/Handlers/MFRHandler.java
+++ java/com/github/coolsquid/SquidUtils/Handlers/MFRHandler.java
@@ -20,7 +20,7 @@ public class MFRHandler {
 		if (event.Name == "Greggy_greg_do_please_kindly_stuff_a_sock_in_it_xxx") {
 			int a = 1;
 			while (a <= 20) {
-				LogHandler.warn("Skyggy_sky_do_please_kindly_stuff_a_sock_in_it_xxx");
+				LogHandler.warn("Skyggy_sky_do_please_kindly_shut_up");
 				a++;
 			}
 			//RecipeHandler.OreDictRecipe("Greggy_greg_do_please_kindly_stuff_a_sock_in_it_xxx");

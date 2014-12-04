322ecce0e5af2511c6dc696f249048430daca129
coolsquid <snakkeflink@gmail.com>
2014-12-04 15:30:31 +0100

    Update ModHandler.java

--- java/com/github/coolsquid/SquidUtils/Handlers/ModHandler.java
+++ java/com/github/coolsquid/SquidUtils/Handlers/ModHandler.java
@@ -1,12 +1,6 @@
 package com.github.coolsquid.SquidUtils.Handlers;
 
 import net.minecraftforge.oredict.OreDictionary;
-import cpw.mods.fml.common.FMLCommonHandler;
-import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-
-package com.github.coolsquid.SquidUtils.Handlers;
-
-import net.minecraftforge.oredict.OreDictionary;
 //import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
@@ -23,17 +17,15 @@ public class ModHandler {
 	public void oredict(OreDictionary.OreRegisterEvent event) {
 		if (event.Name == "Greggy_greg_do_please_kindly_stuff_a_sock_in_it_xxx") {
 			int a = 1;
-			while (a <= 5) {
+			while (a <= 20) {
 				LogHandler.warn("Skyggy_sky_do_please_kindly_shut_up");
 				a++;
 			}
 			//RecipeHandler.OreDictRecipeBlock("Greggy_greg_do_please_kindly_stuff_a_sock_in_it_xxx", Blocks.diamond_block);
 		}
-		/**
-		else if (event.Name.length() > 25) {
-			LogHandler.bigWarning("Too long oredictionary entry! This is not allowed due to SquidCore.");
-			FMLCommonHandler.instance().exitJava(1, false);
+		else if (event.Name.length() > 20) {
+			LogHandler.bigWarning("Too long oredictionary entry!");
+			LogHandler.info("Oredictionary entry " + event.Name + " is too long. Please do not use more than 20 letters.");
 		}
-		*/
 	}
 }

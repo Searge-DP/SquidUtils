d7f97636cdc6b09470e14f1625aef24b9cfc7b1b
coolsquid <snakkeflink@gmail.com>
2014-12-04 21:06:52 +0100

    Fix stuff.

--- java/com/github/coolsquid/SquidUtils/Utils/Utils.java
+++ java/com/github/coolsquid/SquidUtils/Utils/Utils.java
@@ -2,6 +2,8 @@ package com.github.coolsquid.SquidUtils.utils;
 
 import java.util.Random;
 
+import cpw.mods.fml.common.Loader;
+
 /**
 * 
 * @author CoolSquid
@@ -15,12 +17,21 @@ public class Utils {
 	
 	public static boolean chance(int D, int K) {
 
-		int A = rand.nextInt(D) + 1;
-		if (A <= K) {
+		int A = rand.nextInt(K) + 1;
+		if (A <= D) {
 			return true;
 		}
 		else {
 			return false;
+		}
+	}
+	
+	public static boolean isModNotLoaded(String mod) {
+		if (Loader.isModLoaded(mod)) {
+			return false;
+		}
+		else {
+			return true;
 		}
 	}
 }

a20eec7f2fb31a96acf7d948deeeb32b65cc0154
coolsquid <sankkeflink@gmail.com>
2014-12-16 21:28:44 +0100

    Version number.

--- java/com/github/coolsquid/SquidUtils/Reference.java
+++ java/com/github/coolsquid/SquidUtils/Reference.java
@@ -10,7 +10,7 @@ package com.github.coolsquid.SquidUtils;
 public class Reference {
 	
 	public static final String modid = "SquidUtils";
-	public static final String version = "1.0.7";
+	public static final String version = "1.0.8";
 	public static final String forum = "http://bit.ly/squidutilsforum";
 	public static final String dependencies = "required-after" + "";
 }

bc763e4b40bbea145fd335fb25915394ebe510e7
coolsquid <snakkeflink@gmail.com>
2014-12-20 18:44:04 +0100

    Version number

--- java/com/github/coolsquid/SquidUtils/Utils/Data.java
+++ java/com/github/coolsquid/SquidUtils/Utils/Data.java
@@ -14,7 +14,7 @@ public class Data {
 	public static final boolean developmentEnvironment = (Boolean)Launch.blackboard.get("fml.deobfuscatedEnvironment");
 	
 	public static final String modid = "SquidUtils";
-	public static final String version = "1.1.0b";
+	public static final String version = "1.1.1";
 	public static final String forum = "http://bit.ly/squidutilsforum";
 	public static final String author = "CoolSquid";
 }

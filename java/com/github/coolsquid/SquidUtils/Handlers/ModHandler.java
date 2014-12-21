3b05259086e9d6982167641b9a14249b99623145
coolsquid <snakkeflink@gmail.com>
2014-12-21 11:15:02 +0100

    .

--- java/com/github/coolsquid/SquidUtils/Handlers/ModHandler.java
+++ java/com/github/coolsquid/SquidUtils/Handlers/ModHandler.java
@@ -22,12 +22,12 @@ public class ModHandler {
 	public void oreDictHandler(OreDictionary.OreRegisterEvent event) {
 		if (event.Name == "Greggy_greg_do_please_kindly_stuff_a_sock_in_it") {
 			int a = 1;
-			while (a <= ConfigHandler.getMFR()) {
+			while (a <= ConfigHandler.MFR) {
 				LogHelper.warn("Skyggy_sky_do_please_kindly_shut_up");
 				a++;
 			}
 		}
-		if (event.Name.length() > 20 && ConfigHandler.getOreDictComplain()) {
+		if (event.Name.length() > 20 && ConfigHandler.OreDictComplain) {
 			LogHelper.bigWarning(Level.WARN, "Oredictionary entry \"" + event.Name + "\" is very long. Please do not use more than 20 symbols.");
 		}
 	}

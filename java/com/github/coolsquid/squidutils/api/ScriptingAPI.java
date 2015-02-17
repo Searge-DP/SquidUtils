dad94e3c6944b5f0cb087997751e709e6a3456c1
coolsquid <snakkeflink@gmail.com>
2015-02-17 02:33:07 +0100

    Update ScriptingAPI.java

--- java/com/github/coolsquid/squidutils/api/ScriptingAPI.java
+++ java/com/github/coolsquid/squidutils/api/ScriptingAPI.java
@@ -11,7 +11,7 @@ public class ScriptingAPI {
 	
 	public static final HashMap<String, IEventAction> actions = new HashMap<String, IEventAction>();
 	public static final HashMap<String, IEventTrigger> triggers = new HashMap<String, IEventTrigger>();
-	public static final HashMap<String, IEventArgument> arguments = new HashMap<String, IEventOption>();
+	public static final HashMap<String, IEventArgument> arguments = new HashMap<String, IEventArgument>();
 	
 	public static void add(String name, IEventAction action) {
 		actions.put(name, action);
